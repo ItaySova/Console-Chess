e35546edc503241d43420bacbf8d3dcc72863d39
Itay <itay3742@gmail.com>
2024-05-07 08:23:45 +0300

    init tests for moves

---
+++ Tests/GetMovesTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Chess.Tests
+{
+    internal class GetMovesTest
+    {
+
+        public static void GetMovesTestPawm()
+        {
+            //
+        }
+
+        public static void GetMovesTestking()
+        {
+            //
+        }
+
+        public static void GetMovesTestQueen()
+        {
+            //
+        }
+
+        public static void GetMovesTestBishop()
+        {
+            //
+        }
+
+        public static void GetMovesTestRook()
+        {
+            //
+        }
+
+        public static void GetMovesTestKnight()
+        {
+            //
+        }
+    }
+}
