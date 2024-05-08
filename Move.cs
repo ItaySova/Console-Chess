744af74283fea367dd95298ee4d455831e7ba304
Itay <itay3742@gmail.com>
2024-05-08 13:41:39 +0300

    Update Move.cs

--- Move.cs
+++ Move.cs
@@ -23,6 +23,21 @@ namespace Console_Chess
         }
 
         public Position GetToPosition() { return ToPosition; }
+
+        public static Move ConvertStringToMove(string moveString)
+        {
+            Position fromPos = Position.ConvertCharsToPosition(moveString[0], moveString[1]);
+            Position toPose = Position.ConvertCharsToPosition(moveString[2], moveString[3]);
+
+            return new Move(fromPos, toPose);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Move other = obj as Move;
+            if (other == null) return false;
+            return other.FromPosition == this.FromPosition && other.ToPosition == this.ToPosition;
+        }
         public override string ToString()
         {
             return FromPosition.ToString() + ToPosition.ToString();
