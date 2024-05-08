35aa7e5c7f746fa9a100bc748765cc00598573b8
Itay <itay3742@gmail.com>
2024-05-08 10:36:56 +0300

    Create GameState.cs

---
+++ GameState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Chess
+{
+    internal class GameState
+    {
+        bool TurnPlayer; // 
+        bool GameOver; // is the game over
+        bool IsCheck; // check for turn player
+        int turnCount; //
+        string BoardHistoryFirstTime;
+        string BoardHistorySecondTime;
+        string BoardHistoryThirdTime;
+
+        public GameState() { }
+
+        public bool IsGameOver(Board board)
+        {
+            // first implement Checkmate game over only!
+            return false;
+        }
+    }
+}
