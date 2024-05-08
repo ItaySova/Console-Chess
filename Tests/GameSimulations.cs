3a4f2dcaf3071ceaeaa61ba0519b5bdd6ea4e295
Itay <itay3742@gmail.com>
2024-05-08 14:42:13 +0300

    Update GameSimulations.cs

--- Tests/GameSimulations.cs
+++ Tests/GameSimulations.cs
@@ -13,13 +13,32 @@ namespace Console_Chess.Tests
 
         public GameSimulations() { }
 
-        public static void Simulation1()
+        public static void Simulation1(string option)
         {
             //ChessGame testGame = new GameSimulations();
             string[] inputs = { "e2e4", "f7f5", "b1c3", "b8c6" ,"d1h5","g7g6","h5f5","g6f5"};
+            string[] inputsForQuickCheckmate = { "e2e4", "f7f5", "d2d3", "g7g5", "d1h5"};
+
+            string[] inputsOfChoice = null;
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("running random simulation");
+                    inputsOfChoice = inputs;
+                    break;
+                case "2":
+                    Console.WriteLine("running quick checkmate simulation");
+                    inputsOfChoice = inputsForQuickCheckmate;
+                    break;
+                case "3":
+                    Console.WriteLine(" not implemented yet");
+                    break;
+                default:
+                    break;
+            }
             //testGame.GameSimulation(inputs);
             GameSimulations simu = new GameSimulations();
-            simu.GameSimulationWithInputCheck(inputs);
+            simu.GameSimulationWithInputCheck(inputsOfChoice);
         }
 
         public override void GameSimulation(string[] InputMoves)
@@ -45,7 +64,7 @@ namespace Console_Chess.Tests
                 bool isMoveValid = IsMoveInAllPlayerMoves(playerMove);
                 Console.WriteLine((isMoveValid ? "execute input:" : "invalid move"));
                 // executing the input
-                ExecuteMove(playerMove);
+                ExecuteMove(testBoard,playerMove);
                 // change the turn player and incrementing turn count:
                 Console.WriteLine("press enter to continue");
                 Console.ReadLine();
@@ -66,44 +85,74 @@ namespace Console_Chess.Tests
         {
             Console.WriteLine("playing from moves test!");
             Board testBoard = GetBoard();
-            bool Player = GetTurnPlayer();
             bool gameOver = false;
             int moveCounter = 0;
-            while (!gameOver)
+            GameState state = new GameState();
+            bool ContinueManually = false;
+            while (!state.GetGameOver())
             {
-
-                // printing
-                testBoard.Print();
-                bool checkTest = isPlayerInCheck();
-
-                // taking user input
-                Move playerMove = ValidateAutomationInput(inputs[moveCounter]);
-                Console.WriteLine(playerMove.ToString() + " by player: " + (Player ? "white" : "black"));
-                Console.WriteLine((checkTest ? "player is in Check" : "player is not in check!"));
-
-                // testing the input for valid input - rulewise
-                bool isMoveValid = IsMoveInAllPlayerMoves(playerMove);
-                Console.WriteLine((isMoveValid ? "execute input:" : "invalid move, repeat with new input"));
-                if(!isMoveValid)
-                {
-                    Console.WriteLine("enter new input: ");
-                    changeMoveIfNotInMovesList(inputs, moveCounter);
-                    continue;
-                }
-                // executing the input
-                ExecuteMove(playerMove);
-                // change the turn player and incrementing turn count:
-                Console.WriteLine("press enter to continue");
-                Console.ReadLine();
-                moveCounter++;
-                if (moveCounter == inputs.Length)
+                // check if simulation run out of moves
+                if (moveCounter == inputs.Length && ContinueManually == false)
                 {
                     testBoard.Print();
-                    Console.WriteLine("ran out of automatic inputs");
-                    gameOver = true;
+                    Console.WriteLine("ran out of automatic inputs, would you like to continue manually?");
+                    string answer = Console.ReadLine();
+                    switch (answer)
+                    {
+                        case "y":
+                            Console.WriteLine("will be implemented");
+                            ContinueManually = true;
+                            break;
+                        case "n":
+                            Console.WriteLine("ending simulation");
+                            break;
+                    }
+                    if (!ContinueManually)
+                    {
+                        break;
+                    }
                 }
-                Player = !Player;
-                SetTurnPlayer(Player);
+
+
+                bool Player = state.GetPlayer();
+                string[] MovesAvailable = GetAllPlayerMoves();
+                // printing
+                testBoard.Print();
+                Console.WriteLine(state.GetCheckStatus() ? "CHECK" : "");
+                string[] LegalMoves = state.GetLegalMoves(testBoard, MovesAvailable);
+
+                bool isMoveValid = false;
+                Move playerMove = null;
+                // taking user input
+                while (!isMoveValid)
+                {
+                    playerMove = ValidateAutomationInput(inputs[moveCounter]);
+                    isMoveValid = Array.IndexOf(MovesAvailable, playerMove.ToString()) != -1;
+                    if (!isMoveValid)
+                    {
+                        Console.WriteLine("input not valid - enter new input: ");
+                        changeMoveIfNotInMovesList(inputs, moveCounter);
+                        continue;
+                    }
+                    if (isMoveValid)
+                    {
+                        Console.WriteLine("move is in all possible - check for legal moves");
+                        isMoveValid = Array.IndexOf(LegalMoves, playerMove.ToString()) != -1;
+                    }
+                }
+                    
+
+                // executing the input
+                ExecuteMove(testBoard,playerMove);
+                // change the turn player and incrementing turn count:
+                Console.WriteLine((state.GetPlayer() ? "white" : "black") + " chose the move " + playerMove.ToString());
+                Console.WriteLine("press enter to continue");
+                Console.ReadLine();
+                state.UpdateGameState(testBoard);
+                moveCounter++;
+                
+                // Player = !Player;
+                SetTurnPlayer(state.GetPlayer());
             }
         }
 
