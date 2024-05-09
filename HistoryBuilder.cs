09a54b18081900cb1110e083bdf794d5c1a1ca21
Itay <itay3742@gmail.com>
2024-05-09 15:26:44 +0300

    Create HistoryBuilder.cs

---
+++ HistoryBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Chess
+{
+    internal class HistoryBuilder
+    {
+        public static string SnapshotBoard(Board board, GameState state)
+        {
+            string Snapshot = "";
+            // add board raw representation
+            Snapshot += board.ToString();
+            // add castling rights for both players as KSW KSB - king side white, king side black
+            
+            // add castling rights for both players as QSW QSB - queen side white, queen side black
+
+            // add En Passant Position as tostring position or nothing 
+            return Snapshot;
+        }
+
+        // helper for KSW KSB QSW QSB
+        public static string CastlingRights(Board board, GameState state)
+        {
+            string whiteRights = "";
+            string blackRights = "";
+
+            return whiteRights + blackRights;
+        }
+    }
+}
