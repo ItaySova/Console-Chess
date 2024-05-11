3272da0060340c00cf273f992fb6a46bad3f45fe
Itay <itay3742@gmail.com>
2024-05-11 15:21:54 +0300

    Update ChessGame.cs

--- ChessGame.cs
+++ ChessGame.cs
@@ -393,7 +393,7 @@ namespace Console_Chess
         {
             Console.WriteLine("hello and welcome to my chess console game!\nhere are the following rules:\n" +
                 "1. at the begining of the turn, the turn player choose a move in the format of 1 letter for the column and a number, representing" +
-                "the piece on the board he choose to make a move with, and then 1 letter and one number for representing the place the piece" +
+                " the piece on the board he choose to make a move with, and then 1 letter and one number for representing the place the piece" +
                 "moves to. for example 'a2a4' or 'e2e4.'" +
                 "a player must choose its own pieces.\n2. a player can forfeit the match by entering 'RESIGN' instead of his move, resulting" +
                 "in a loss.\n3. if a player want to look againg at those rules - type 'RULES' instead of the move");
